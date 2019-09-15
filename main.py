303983acde51912c644dc8d621b9fcf7479b8bb4
xhn16729 <xiong_haonan@163.com>
2019-09-15 19:56:30 +0800

    rewrite repeat func

--- main.py
+++ main.py
@@ -25,35 +25,35 @@ try:
 	last_time_forwarded_cao = 0
 	last_time_repeated = 0
 	last_time_greeting = 0
+	last_msg = ""
 	@bot.message_handler(func=lambda message: True)
 	def all(message):
 		global last_time_forwarded_cao
 		global last_time_repeated
 		global last_time_greeting
+		global last_msg
 		curr_time = time.time()
 		randNum = random.randint(0, 100)
 
-		if randNum % 10 == 0 and curr_time - last_time_repeated >= 600:
+		if last_msg == message.text and curr_time - last_time_repeated >= 60:
 			last_time_repeated = time.time()
-			bot.reply_to(message, message.text)
-		
-		if message.text == '草' and curr_time - last_time_forwarded_cao >= 300:
+			bot.forward_message(message.chat.id, message.chat.id, message.id)
+		last_msg = message.text
+
+		if message.text == '草' and curr_time - last_time_forwarded_cao >= 30:
 			last_time_forwarded_cao = time.time()
 			if randNum % 2 == 0:
-				bot.reply_to(message, '草')
+				bot.send_message(message.chat.id, '草')
 			else:
 				bot.send_sticker(message.chat.id, 'CAADBQADawADvXbGBYun-zdWQQwmFgQ')
 		
 		if message.text[0] == "/" and message.reply_to_message != None and message.reply_to_message.from_user.username != "littlebear_group_helper_bot" and message.reply_to_message.from_user.username != message.from_user.username and len(message.text) <= 10:
 			send_name = str(message.from_user.first_name)
 			reply_name = str(message.reply_to_message.from_user.first_name)
-			if randNum % 3 == 0:
-				text = '[{send_name}](tg://user?id={from_id}) {movement}  了 [{reply_name}](tg://user?id={reply_id}) ！{emoji}'.format(send_name=send_name, from_id=str(message.from_user.id), movement=message.text[1::], reply_name=reply_name, reply_id=str(message.reply_to_message.from_user.id), emoji=emoji[randNum % (len(emoji)-1)])
+			if randNum % 5 == 0:
+				text = '{send_name} {movement} 了 {reply_name} ！{emoji}'.format(send_name=send_name, movement=message.text[1::], reply_name=reply_name, emoji=emoji[randNum % (len(emoji)-1)])
 				bot.reply_to(message, text, parse_mode="Markdown")
-			else:
-				text = '[{send_name}](tg://user?id={from_id}) {movement}  失败了 [{reply_name}](tg://user?id={reply_id}) ！{emoji}'.format(send_name=send_name, from_id=str(message.from_user.id), movement=message.text[1::], reply_name=reply_name, reply_id=str(message.reply_to_message.from_user.id), emoji=emoji[randNum % (len(emoji)-1)])
-				bot.reply_to(message, text, parse_mode="Markdown")
-		
+
 		if message.from_user.id == 704759255 and curr_time - last_time_greeting >= 300:
 			last_time_greeting = time.time()
 			bot.reply_to(message, '给奶奶请安！')
